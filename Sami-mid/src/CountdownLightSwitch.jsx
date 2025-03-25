c80cfd557bd3cae18cae07ca85db62ca7b9094fa
Samiullah-Sangi <samisangi387@gmail.com>
2025-03-24 22:19:23 -0700

    i forgot third commit so this is combine 3 and 4 commint add countdown and timer and theme functionality

--- Sami-mid/src/CountdownLightSwitch.jsx
+++ Sami-mid/src/CountdownLightSwitch.jsx
@@ -1,5 +1,36 @@
-import { useState } from "react"
-function CountdownLightSwitch(){
+import { useState,useEffect } from "react"
+
+function CountdownLightSwitch()
+{
+    const [islightMode,setislightMode]=useState(false);
+
+    const[countdown,setCountdown]=useState(30);
+    
+    const[isrunning,setisrunning]=useState(false);
+   
+    useEffect(()=>{
+        let timer;
+        if(isrunning && countdown > 0){
+            timer =setTimeout(()=> setCountdown(countdown-1),1000);
+        }
+    else if (countdown ===0){
+        setisrunning(false)
+
+     } 
+     return()=>clearTimeout(timer)
+    },[countdown,isrunning]);
+
+    const handlethemetoggle = ()=>{
+        setislightMode((prevMode)=>!preMode)
+    }
+
+    const handlestart=()=>{
+        if(!isrunning) setisrunning(true);
+    };
+    
+    const handlesliderchange = (event)=>{
+        if(!isrunning) setCountdown(parseInt(event.target.value));
+    };
     return(
         <>
         <h1>Countdown & light Switch</h1>
